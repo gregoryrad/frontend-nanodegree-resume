d62eae7c9b2e703386d1721d5baef22d84bdb8a8
gregoryrad <greg.ray.radcliffe@gmail.com>
2017-04-13 20:32:57 -0700

    Added Work Experience

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -17,6 +17,22 @@ var bio = {
     "bioPic": "images/GregoryR_profilePic.jpeg"
 }
 
+var work = {
+    "workTitle": "Principal",
+    "workEmployer": "Creative Hubbub",
+    "workDates": "Apr. 2000 - Present",
+    "workLocation": "Seattle",
+    "workDescription": "Donec non nibh ullamcorper, faucibus orci quis, pharetra urna. Sed imperdiet turpis gravida quam vestibulum scelerisque. Nam rhoncus nulla vel sem rutrum, ut porttitor justo tempus. Integer et arcu id risus commodo fringilla"
+}
+
+var education = {
+    "schoolName": "Edmonds Community College",
+    "schoolDegree": "Certificate in Web App Development",
+    "schoolDates": "Sep. 2014 - Jun. 2016",
+    "schoolCity": "Lynnwood, WA"
+}
+
+
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 var formattedMobile = HTMLmobile.replace("%data%", bio.contactInfo.mobile);
@@ -27,15 +43,36 @@ var formattedLocation = HTMLlocation.replace("%data%", bio.contactInfo.location)
 var formattedPicURL = HTMLbioPic.replace("%data%", bio.bioPic);
 var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
 var formattedSkills = HTMLskills.replace("%data%", bio.skills);
+var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.workTitle);
+var formattedWorkEmployer = HTMLworkEmployer.replace("%data%", work.workEmployer);
+var formattedWorkDates = HTMLworkDates.replace("%data%", work.workDates);
+var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.workLocation);
+var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.workDescription);
+var formattedSchoolName = HTMLschoolName.replace("%data%", education["schoolName"]);
+var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education["schoolDegree"]);
+var formattedSchoolDates = HTMLschoolDates.replace("%data%", education["schoolDates"]);
+var formattedSchoolCity = HTMLschoolLocation.replace("%data%", work["schoolDates"]);
 
 
-$("#header").prepend(formattedSkills);
-$("#header").prepend(formattedWelcomeMsg);
-$("#header").prepend(formattedPicURL);
-$("#header").prepend(formattedLocation);
-$("#header").prepend(formattedGithub);
-$("#header").prepend(formattedTwitter);
-$("#header").prepend(formattedEmail);
-$("#header").prepend(formattedMobile);
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+$("#topContacts").append(formattedMobile);
+$("#topContacts").append(formattedEmail);
+$("#topContacts").append(formattedTwitter);
+$("#topContacts").append(formattedGithub);
+$("#topContacts").append(formattedLocation);
+$("#topContacts").append(formattedPicURL);
+$("#topContacts").append(formattedWelcomeMsg);
+$("#topContacts").append(formattedSkills);
+$("#workExperience").append(formattedWorkEmployer);
+$("#workExperience").append(formattedWorkDates);
+$("#workExperience").append(formattedWorkLocation);
+$("#workExperience").append(formattedWorkDescription);
+$("#education").append(formattedSchoolName);
+$("#education").append(formattedSchoolDegree);
+$("#education").append(formattedSchoolDates);
+$("#education").append(formattedSchoolCity);
+
+
+
+$("#workExperience").append(formattedWorkTitle);
