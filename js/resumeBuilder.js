3e78938e5acabbae1aa714548bd2732c82dfe06c
Greg Radcliffe <greg.ray.radcliffe@gmail.com>
2017-04-18 10:48:00 -0700

    Added functions for displaying work and projects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,59 +1,110 @@
-// var name = "Gregory Radcliffe";
-// var role = "Front End Developer";
-
+/* ---------------- bio object ---------------- */
 var bio = {
     "name": "Gregory Radcliffe",
     "role": "Front End Web Developer",
-    "contactInfo": {
+    "contacts": {
         "mobile": "(206)920-8376",
         "email": "mailto:greg.ray.radcliffe@gmail.com",
-        "twitter": "https://twitter.com/greg_radcliffe",
         "github": "https://github.com/gregoryrad",
-        "location": "Portland, OR"},
+        "twitter": "https://twitter.com/greg_radcliffe",
+        "location": "Portland, OR"
+    },
     "welcomeMessage": "Hi there. This is my welcome message. Aenean ac ullamcorper arcu. In sed fermentum purus. Ut aliquam tempor odio, non commodo turpis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec volutpat facilisis ultrices. Donec et diam lobortis, venenatis dui id, viverra urna. Aliquam sed magna diam. Maecenas sed nisi ac sapien condimentum dignissim eget nec leo. Nunc consectetur arcu vitae elit condimentum sollicitudin. Phasellus eu metus ex.",
     "skills": [
         "PhotoShop", "Illustrator", "HTML", "CSS", "Responsive Design"
     ],
-    "bioPic": "images/GregoryR_profilePic.jpeg"
-}
+    "biopic": "images/GregoryR_profilePic.jpeg"
+};
 
-var work = {
-    "workTitle": "Principal",
-    "workEmployer": "Creative Hubbub",
-    "workDates": "Apr. 2000 - Present",
-    "workLocation": "Seattle",
-    "workDescription": "Donec non nibh ullamcorper, faucibus orci quis, pharetra urna. Sed imperdiet turpis gravida quam vestibulum scelerisque. Nam rhoncus nulla vel sem rutrum, ut porttitor justo tempus. Integer et arcu id risus commodo fringilla"
-}
 
+/* ---------------- education object ---------------- */
 var education = {
-    "schoolName": "Edmonds Community College",
-    "schoolDegree": "Certificate in Web App Development",
-    "schoolDates": "Sep. 2014 - Jun. 2016",
-    "schoolCity": "Lynnwood, WA"
-}
+    "schools": [
+        {
+            "name": "Edmonds Community College",
+            "location": "Lynnwood, WA",
+            "degree": "Certificate in Web App Development",
+            "major": "CompSci",
+            "dates": "Sep. 2014 - Jun. 2016"
+        }
+    ],
+    "onlineCourses": [
+        {
+            "title": "Frontend Nanodegree",
+            "school": "Udacity",
+            "dates": "Mar. 2017 - Present",
+            "url": "Lynnwood, WA"
+        }
+    ]
+};
+
+/* ---------------- work object ---------------- */
+var work = {
+    "jobs": [
+        {
+            "title": "Principal",
+            "employer": "Creative Hubbub",
+            "dates": "Apr. 2000 - Present",
+            "location": "Seattle, WA",
+            "description": "Donec non nibh ullamcorper, faucibus orci quis, pharetra urna. Sed imperdiet turpis gravida quam vestibulum scelerisque. Nam rhoncus nulla vel sem rutrum, ut porttitor justo tempus. Integer et arcu id risus commodo fringilla"
+        },
+        {
+            "title": "Wed Producer",
+            "employer": "Rory Martin, Inc.",
+            "dates": "Sep. 2014 - Sep. 2016",
+            "location": "Seattle, WA",
+            "description": "Donec non nibh ullamcorper, faucibus orci quis, pharetra urna. Sed imperdiet turpis gravida quam vestibulum scelerisque. Nam rhoncus nulla vel sem rutrum, ut porttitor justo tempus. Integer et arcu id risus commodo fringilla"
+        },
+        {
+            "title": "UI Designer",
+            "employer": "WildTangent",
+            "dates": "Nov. 2013 - Jun. 2016",
+            "location": "Redmond, WA",
+            "description": "Donec non nibh ullamcorper, faucibus orci quis, pharetra urna. Sed imperdiet turpis gravida quam vestibulum scelerisque. Nam rhoncus nulla vel sem rutrum, ut porttitor justo tempus. Integer et arcu id risus commodo fringilla"
+        }
+    ]
+};
+
+/* ---------------- projects object ---------------- */
+var projects = {
+    "projects": [
+        {
+            "title": "Suspendisse vitae",
+            "dates": "2017",
+            "description": "Pellentesque molestie pellentesque neque. Morbi cursus odio ante, eget suscipit metus auctor vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            "image": [
+                "images/fpo-1.jpg",
+                "images/fpo-2.jpg"
+            ]
+        },
+        {
+            "title": "Donec diam",
+            "dates": "2016",
+            "description": "Morbi in ipsum non velit ornare tristique. Maecenas vel lacus id turpis pharetra hendrerit. Maecenas ipsum erat, cursus a mollis a, rutrum sit amet lacus.",
+            "image": [
+                "images/fpo-2.jpg",
+                "images/fpo-1.jpg"
+                ]
+        }
+    ]
+};
 
 
+
+
+// bio formatted variables
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-var formattedMobile = HTMLmobile.replace("%data%", bio.contactInfo.mobile);
-var formattedEmail = HTMLemail.replace("%data%", bio.contactInfo.email);
-var formattedTwitter = HTMLtwitter.replace("%data%", bio.contactInfo.twitter);
-var formattedGithub = HTMLgithub.replace("%data%", bio.contactInfo.github);
-var formattedLocation = HTMLlocation.replace("%data%", bio.contactInfo.location);
-var formattedPicURL = HTMLbioPic.replace("%data%", bio.bioPic);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+var formattedPicURL = HTMLbioPic.replace("%data%", bio.biopic);
 var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-var formattedSkills = HTMLskills.replace("%data%", bio.skills);
-var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.workTitle);
-var formattedWorkEmployer = HTMLworkEmployer.replace("%data%", work.workEmployer);
-var formattedWorkDates = HTMLworkDates.replace("%data%", work.workDates);
-var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.workLocation);
-var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.workDescription);
-var formattedSchoolName = HTMLschoolName.replace("%data%", education["schoolName"]);
-var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education["schoolDegree"]);
-var formattedSchoolDates = HTMLschoolDates.replace("%data%", education["schoolDates"]);
-var formattedSchoolCity = HTMLschoolLocation.replace("%data%", work["schoolDates"]);
 
 
+// bio jQuery display
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 $("#topContacts").append(formattedMobile);
@@ -61,18 +112,78 @@ $("#topContacts").append(formattedEmail);
 $("#topContacts").append(formattedTwitter);
 $("#topContacts").append(formattedGithub);
 $("#topContacts").append(formattedLocation);
-$("#topContacts").append(formattedPicURL);
-$("#topContacts").append(formattedWelcomeMsg);
-$("#topContacts").append(formattedSkills);
-$("#workExperience").append(formattedWorkEmployer);
-$("#workExperience").append(formattedWorkDates);
-$("#workExperience").append(formattedWorkLocation);
-$("#workExperience").append(formattedWorkDescription);
-$("#education").append(formattedSchoolName);
-$("#education").append(formattedSchoolDegree);
-$("#education").append(formattedSchoolDates);
-$("#education").append(formattedSchoolCity);
+$("#header").append(formattedPicURL);
+$("#header").append(formattedWelcomeMsg);
+
+if (bio.skills.length > 0) {
+
+    $("#header").append(HTMLskillsStart);
+
+    var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+    $("#skills").append(formattedSkill);
+    formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+    $("#skills").append(formattedSkill);
+    formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+    $("#skills").append(formattedSkill);
+    formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
+    $("#skills").append(formattedSkill);
+};
+
+/* ---------------- work display function ---------------- */
+ // TODO: debug result of undefined in output
+work.jobs.display = function () {
+    function showWork() {
+        work.jobs.forEach(function(job) {
+            $("#workExperience").append(HTMLworkStart);
+            var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs.title);
+            var formattedWorkEmployer = HTMLworkEmployer.replace("%data%", work.jobs.employer);
+            var formatedEmployerTitle = formattedWorkEmployer + formattedWorkTitle;
+            $(".work-entry:last").append(formatedEmployerTitle);
+
+            var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs.dates);
+            $(".work-entry:last").append(formattedWorkDates);
+
+            var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs.location);
+            $(".work-entry:last").append(formattedWorkLocation);
+
+            var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs.description);
+            $(".work-entry:last").append(formattedWorkDescription);
+        });
+    }showWork();
+};
+
+work.jobs.display();
 
 
+/* ---------------- projects display function ---------------- */
+ // TODO: debug result of undefined in output
+projects.projects.display = function() {
+    function showProject() {
+        projects.projects.forEach(function(project) {
+            $("#projects").append(HTMLprojectStart);
 
-$("#workExperience").append(formattedWorkTitle);
+            var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.title);
+            $(".project-entry:last").append(formattedProjectTitle);
+
+            var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.dates);
+            $(".project-entry:last").append(formattedProjectDates);
+
+            var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.description);
+            $(".project-entry:last").append(formattedProjectDescription);
+
+            function showImage() {
+                projects.projects.forEach(function(image){
+                    var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.image);
+                    $(".project-entry:last").append(formattedProjectImage);
+                });
+            }
+            showImage();
+        });
+    }
+    showProject();
+};
+
+projects.projects.display();
+
+
+// $("mapDiv").append(googleMap);
